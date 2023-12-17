16c843ac51c9f7e4ff6180d9dc097a9b324d7dc6
Breno Yuri <byurifb@gmail.com>
2023-12-17 04:45:29 -0300

    Validações Corrigidas

--- rest-project/app/src/main/java/br/albatross/agenda/domain/models/contato/DadosParaAtualizacaoDeContatoDto.java
+++ rest-project/app/src/main/java/br/albatross/agenda/domain/models/contato/DadosParaAtualizacaoDeContatoDto.java
@@ -1,23 +1,27 @@
 package br.albatross.agenda.domain.models.contato;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public record DadosParaAtualizacaoDeContatoDto(
 
-		@NotNull
+		@Positive
 		Short id,
 
 		@NotBlank
 		String nome,
 
-		@NotBlank
+		@Pattern(regexp = "\\d{10,13}")
 		String ramal,
 
 		@NotBlank
+		@Size(max = 50)
 		String setor,
 
 		@NotBlank
+		@Size(max = 10)
 		String andar
 	) {
 
--- rest-project/app/src/main/java/br/albatross/agenda/domain/models/contato/DadosParaCadastroDeNovoContatoDto.java
+++ rest-project/app/src/main/java/br/albatross/agenda/domain/models/contato/DadosParaCadastroDeNovoContatoDto.java
@@ -1,24 +1,24 @@
 package br.albatross.agenda.domain.models.contato;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record DadosParaCadastroDeNovoContatoDto(
 
 		@NotBlank
-		@Max(50)
+		@Size(max = 50)
 		String nome,
 
 		@Pattern(regexp = "\\d{10,13}")
 		String ramal,
 
 		@NotBlank
-		@Max(50)
+		@Size(max = 50)
 		String setor,
 
 		@NotBlank
-		@Max(10)
+		@Size(max = 10)
 		String andar
 
 	) {
