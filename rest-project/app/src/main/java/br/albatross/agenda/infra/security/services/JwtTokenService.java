0676b98c00cb27bf9d04783eca3cae991df23fed
Breno Yuri <byurifb@gmail.com>
2024-01-02 12:23:56 -0300

    Fazendo alguns ajustes xD
    Fazendo alguns ajustes nas configs do Wildfly para deploy no Docker.
    
    Validade do Token JWT aumentada para 12h

--- rest-project/app/src/main/java/br/albatross/agenda/infra/security/services/JwtTokenService.java
+++ rest-project/app/src/main/java/br/albatross/agenda/infra/security/services/JwtTokenService.java
@@ -1,5 +1,7 @@
 package br.albatross.agenda.infra.security.services;
 
+import static java.time.LocalDateTime.now;
+
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,7 +10,6 @@ import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 import com.auth0.jwt.JWT;
@@ -20,29 +21,32 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class JwtTokenService {
 
-	private static final String ISSUER   = "Albatross18";
-	private static final String AUDIENCE = "Agenda Rest Service API";
 	private RSAPrivateKey privateKey;
-	
+
+	private static final String ISSUER   = "Albatross18";
+	private static final String AUDIENCE = "Agenda.Rest.Service.API";
+
+	private static final long TOKEN_VALID_HOURS = 12L;
+
 	@PostConstruct
 	void init() {
-		 final String configDir = System.getProperty("jboss.server.config.dir");
-	        final Path keyStore = Path.of(configDir, "agenda.jwt.keystore");
-	        char[] password = "secret".toCharArray();
-	        String alias = "agenda-jwt-auth";
-	        PrivateKey pk = null;
-	        try (InputStream in = Files.newInputStream(keyStore)) {
-	            final KeyStore ks = KeyStore.getInstance("PKCS12");
-	            ks.load(in, password);
-	            Key key = ks.getKey(alias, password);
-	            if (key instanceof PrivateKey) {
-	                pk = (PrivateKey) key;
-	            }
-	        } catch (Exception e) {
-	            throw new ExceptionInInitializerError(e);
-	        }
+		final String configDir = System.getProperty("jboss.server.config.dir");
+		final Path keyStore = Path.of(configDir, "agenda.jwt.keystore");
+		char[] password = "secret".toCharArray();
+		String alias = "agenda-jwt-auth";
+		PrivateKey pk = null;
+		try (InputStream in = Files.newInputStream(keyStore)) {
+			final KeyStore ks = KeyStore.getInstance("PKCS12");
+			ks.load(in, password);
+			Key key = ks.getKey(alias, password);
+			if (key instanceof PrivateKey) {
+				pk = (PrivateKey) key;
+			}
+		} catch (Exception e) {
+			throw new ExceptionInInitializerError(e);
+		}
 
-	        privateKey = (RSAPrivateKey) pk;
+		privateKey = (RSAPrivateKey) pk;
 
 	}
 
@@ -57,17 +61,8 @@ public class JwtTokenService {
 				.sign(Algorithm.RSA256(privateKey));
 	}
 
-	public void validateToken(String jwtToken) {
-		JWT
-			.require(Algorithm.RSA256(privateKey))
-			.withIssuer(ISSUER)
-			.withAudience(AUDIENCE)
-			.build()
-			.verify(jwtToken);
+	private Instant getExpiresAtInstant() {
+		return now().plusHours(TOKEN_VALID_HOURS).toInstant(ZoneOffset.UTC);
 	}
 
-	private Instant getExpiresAtInstant() {
-		return LocalDateTime.now().plusSeconds(10).toInstant(ZoneOffset.of("-03:00"));
-	}
-	
 }
