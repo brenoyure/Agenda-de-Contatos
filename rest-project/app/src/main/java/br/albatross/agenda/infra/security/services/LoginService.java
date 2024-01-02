dc25009801c264d7ae30bd88bed69685ab1180d9
Breno Yuri <byurifb@gmail.com>
2024-01-02 10:43:03 -0300

    Organizando os arquivos do Wildfly(Runtime)
    Organizando os arquivos de CLI do Wildfly para cada tipo de autenticação, JWT ou Http Basic

--- rest-project/app/src/main/java/br/albatross/agenda/infra/security/services/LoginService.java
+++ rest-project/app/src/main/java/br/albatross/agenda/infra/security/services/LoginService.java
@@ -3,7 +3,6 @@ package br.albatross.agenda.infra.security.services;
 import br.albatross.agenda.domain.models.usuario.DadosBasicosDoUsuarioParaExibicaoDto;
 import br.albatross.agenda.infra.security.credentials.Credenciais;
 import br.albatross.agenda.infra.security.dao.LoginDao;
-import br.albatross.agenda.infra.security.services.password.PasswordService;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 
@@ -12,10 +11,7 @@ public class LoginService {
 
 	@Inject
 	private LoginDao dao;
-	
-	@Inject
-	private PasswordService passwordService;
-	
+
 	public DadosBasicosDoUsuarioParaExibicaoDto logar(Credenciais credenciais) {
 		return dao.logar(credenciais);
 	}
