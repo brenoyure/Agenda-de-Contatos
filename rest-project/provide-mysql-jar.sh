c6f26664226cd55313fa384e0bdcaa54f029ab2e
Breno Yuri <byurifb@gmail.com>
2024-01-07 16:55:33 -0300

    MySQL Atualizado para a versão 8.2.0
    Driver do MySQL e imagem/containers/scripts atualizados para utilizarem a versão 8.2.0 do MySQL.

--- rest-project/provide-mysql-jar.sh
+++ rest-project/provide-mysql-jar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FILE=./app/mysql-connector-j-8.0.33.jar
+FILE=./app/mysql-connector-j-8.2.0.jar
 if [ ! -f "$FILE" ]; then
     echo 'Providing MySQL Connector J from Maven Central Repository'
-    curl -o mysql-connector-j-8.0.33.jar https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/8.0.33/mysql-connector-j-8.0.33.jar
-    mv ./mysql-connector-j-8.0.33.jar ./app/mysql-connector-j-8.0.33.jar
+    curl -o mysql-connector-j-8.2.0.jar https://repo1.maven.org/maven2/com/mysql/mysql-connector-j/8.2.0/mysql-connector-j-8.2.0.jar
+    mv ./mysql-connector-j-8.2.0.jar ./app/mysql-connector-j-8.2.0.jar
     echo 'End of MySQL Connector J from Maven Central Repository Provision'
 fi
